b07b0e3a8f50ee819b04d24e8e4a92448fd50b65
shelag95 <chelag24@gmail.com>
2023-01-30 21:31:23 -0500

    actualizandoporcuartavez

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { addTask } from "./components/addTask.js";
 import { displayTasks } from "./components/readTasks.js";
 
@@ -7,14 +6,4 @@ const btn = document.querySelector('[data-form-btn]');
 //Arrow functions o funciones anonimas,da click al boton
 btn.addEventListener('click', addTask);
 
-=======
-import { addTask } from "./components/addTask.js";
-import { displayTasks } from "./components/readTasks.js";
-
-const btn = document.querySelector('[data-form-btn]');
-
-//Arrow functions o funciones anonimas,da click al boton
-btn.addEventListener('click', addTask);
-
->>>>>>> d612996ab994ec2ec7551cfc62e9e1d8af8f3712
 displayTasks();
